2c97ad40b69c9be574ba8746006a5e33af2298cd
jxhao <jasonhao80s@gmail.com>
2021-10-29 17:16:23 -0400

    Update Vine_Review_Analysis.sql

--- Vine_Review_Analysis.sql
+++ Vine_Review_Analysis.sql
@@ -24,32 +24,22 @@ where vine = 'Y'
 select * into novinereview_table from moreuseful_table
 where vine = 'N'
 
---table3
+--The total number of reviews, the number of 5-star reviews, 
+--and the percentage 5-star reviews are calculated for all Vine 
+--and non-Vine reviews (15 pt)
 select
-(select count(*) from vine_table) as Total_Vote_Count,
-(select count(*) from vine_table where star_rating = 5) as "5-Star_Count",
+(select count(*) from moreuseful_table) as Total_Vote_Count,
+(select count(*) from moreuseful_table where star_rating = 5) as "5-Star_Count",
 cast(
-	(select count(*) from vine_table 
-		where star_rating = 5 and vine = 'Y') 
+	(select count(*) from vinereview_table 
+		where star_rating = 5) 
 	as decimal)*100
-	/
-(select count(*) from vine_table 
-	where vine = 'Y') 
-as "5-Star_vine (%)",
-cast((select count(*) from vine_table 
-	  where star_rating = 5 and vine = 'N')
+		/
+	(select count(*) from vinereview_table) 
+	as "5-Star_vine (%)",
+cast((select count(*) from novinereview_table 
+	  where star_rating = 5)
 	 as decimal)*100/
-	(select count(*) from vine_table where vine = 'N') as "5-Star_no_vine （%）"
+	(select count(*) from novinereview_table) as "5-Star_no_vine （%）"
 ;
 
-/* 
---easy to do in 1 select for the first 2 tables, 
---but as the instruction requires, tables are created in each step. 
---below is a better conbined version.
-select * 
-into useful_reviews_w_vine
-from vine_table v
-where total_votes >= 20
-	and vine = 'Y'
-	and helpful_votes*100/total_votes>=50
-;*/
